7db73af00f1d8d8fe5985203df20644f2d7bd12b
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-04-17 17:03:09 +0200

    Added: img for each result

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -19,9 +19,9 @@ export default {
 
 <template>
     <div class="card">
-        <!-- <div class="card-img">
-            <img src= "`https://image.tmdb.org/t/p/w342 ${item.poster_path}`" alt="">
-        </div> -->
+        <div class="card-img">
+            <img :src= "`https://image.tmdb.org/t/p/w342${item.poster_path}`" alt="">
+        </div>
         <div class="card-body">
             <div class="title"><strong>Titolo: </strong>{{item.title}}</div>
             <div class="original-title"><strong>Titolo originale: </strong>{{ item.original_title }}</div>
@@ -37,29 +37,19 @@ export default {
             <div class="vote" v-else-if="item.vote_average >= 1"><strong>Voto: </strong> 1 </div>
             <div class="vote" v-else-if="item.vote_average < 1"><strong>Voto: </strong> 0 </div>
             <div class="star" v-if="item.vote_average >= 9">
-                <font-awesome-icon :icon="['fas', 'star']" />
-                <font-awesome-icon :icon="['fas', 'star']" />
-                <font-awesome-icon :icon="['fas', 'star']" />
-                <font-awesome-icon :icon="['fas', 'star']" />
-                <font-awesome-icon :icon="['fas', 'star']" />
+                <font-awesome-icon :icon="['fas', 'star']" v-for="n in 5" :key="n" />
             </div>
             <div class="star" v-else-if="item.vote_average >= 7">
-                <font-awesome-icon :icon="['fas', 'star']" />
-                <font-awesome-icon :icon="['fas', 'star']" />
-                <font-awesome-icon :icon="['fas', 'star']" />
-                <font-awesome-icon :icon="['fas', 'star']" />
+                <font-awesome-icon :icon="['fas', 'star']" v-for="n in 4" :key="n"/>
                 <font-awesome-icon :icon="['far', 'star']" />
             </div>
             <div class="star" v-else-if="item.vote_average >= 5">
-                <font-awesome-icon :icon="['fas', 'star']" />
-                <font-awesome-icon :icon="['fas', 'star']" />
-                <font-awesome-icon :icon="['fas', 'star']" />
+                <font-awesome-icon :icon="['fas', 'star']" v-for="n in 3" :key="n" />
                 <font-awesome-icon :icon="['far', 'star']" />
                 <font-awesome-icon :icon="['far', 'star']" />
             </div>
             <div class="star" v-else-if="item.vote_average >= 3">
-                <font-awesome-icon :icon="['fas', 'star']" />
-                <font-awesome-icon :icon="['fas', 'star']" />
+                <font-awesome-icon :icon="['fas', 'star']"  v-for="n in 2" :key="n" />
                 <font-awesome-icon :icon="['far', 'star']" />
                 <font-awesome-icon :icon="['far', 'star']" />
                 <font-awesome-icon :icon="['far', 'star']" />
