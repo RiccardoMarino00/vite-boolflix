4fd0edbaa59a554783b5c10fab2edd60f8690f7d
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-04-17 16:37:06 +0200

    Added: star based on vote

--- src/main.js
+++ src/main.js
@@ -9,8 +9,9 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import {faStar} from '@fortawesome/free-regular-svg-icons'
+import {faStar as faStarSolid} from '@fortawesome/free-solid-svg-icons'
 
-library.add(faStar)
+library.add(faStar, faStarSolid)
 
 const app = createApp(App)
 app.component('font-awesome-icon', FontAwesomeIcon)
